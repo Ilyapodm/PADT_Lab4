27eb4813cd6b4dcfc376a2e9bf1e7cd946c0c3af
Ilya Podmarkov <shmell.letitt12@gmail.com>
2026-05-28 11:52:03 +0300

    feat: add generator

--- src/generator.hpp
+++ src/generator.hpp
@@ -2,45 +2,110 @@
 
 #include <cstddef>
 #include <functional>
-#include <optional>
+#include <stdexcept>
 #include "lab_2/sequence.hpp"
+#include "lab_2/option.hpp"
+#include "ordinal.hpp"
 
-template <class T>
-class LazySequence;
 
 template <class T>
 class Generator {
 public:
-    // Создание объекта
-    Generator(LazySequence<T>* owner,
-              std::function<T(Sequence<T>*)> generator_rule);
-
-    Generator(LazySequence<T>* owner,
-              std::size_t index,
-              Sequence<T>* items);
-
-    Generator(const Generator<T>& other);
-
     virtual ~Generator() = default;
+    
+    virtual bool has_next() const = 0;
+    virtual T get_next() = 0;
 
-    // Декомпозиция
-    virtual T get_next();
+    virtual Ordinal length() const = 0;
 
-    virtual bool has_next() const;
+    // habe to redefine ti in child classes
+    virtual bool supports_get_at() const {
+        return false;  
+    }
 
-    virtual std::optional<T> try_get_next();
+    // general logic 'get_at' is set here, in parent class
+    virtual T get_at(const Ordinal& index) {
+        if (!supports_get_at()) {
+            throw std::logic_error("Generator does not support ordinal indexed access");
+        }
 
-    // Операции
+        if (!(index < length())) {
+            throw std::out_of_range("Ordinal index out of range");
+        }
 
-    virtual Generator<T>* append(T item) const;
+        return get_at_impl(index);  // have to redefine it in child classes
+    }
 
-    virtual Generator<T>* append(Sequence<T>* items) const;
+    virtual Option<T> try_get_next() {
+        if (!has_next())
+            return Option<T>();
 
-    virtual Generator<T>* insert(T item) const;
+        return Option<T>(get_next());
+    }
 
-    virtual Generator<T>* insert(Sequence<T>* items) const;
+    virtual Generator<T>* clone() const = 0;
 
-    virtual Generator<T>* remove(T item) const;
-
-    virtual Generator<T>* remove(Sequence<T>* items) const;
+protected:
+    virtual T get_at_impl(const Ordinal& index) {
+        throw std::logic_error("get_at_impl is not implemented");
+    }
 };
+
+// template <typename T>
+// class EmptyGenerator : public Generator<T> {
+//     virtual ~EmptyGenerator() = default;
+
+//     bool has_next() const override { return false; }
+//     T get_next() override { throw std::logic_error("There is no next element in EmptyGenerator"); }
+
+//     Ordinal length() const override { return Ordinal(); }
+// }
+
+// template <class T>
+// class GeneratorFactory {
+// public:
+//     virtual Generator<T>* create() const = 0;
+//     virtual Ordinal length() const = 0;
+
+//     virtual ~GeneratorFactory() = default;
+// };
+
+// template <typename T>
+// class SequenceGenerator : public Generator<T> {
+// public:
+//     explicit SequenceGenerator(const Sequence<T>* source)
+//         : source_(source) {}
+
+//     bool has_next() const override {
+//         return index_ < source_->get_size();
+//     }
+
+//     T get_next() override {
+//         if (!has_next()) {
+//             throw std::out_of_range("Generator is exhausted");
+//         }
+
+//         return source_->get(index_++);
+//     }
+
+//     Ordinal length() const override {
+//         return ordinal(source_->get_size());
+//     }
+
+//     bool supports_get_at() const override {
+//         return true;
+//     }
+
+//     T get_at(const Ordinal& index) override {
+//         if (!index.is_finite()) {
+//             throw std::out_of_range("Infinite index for finite sequence");
+//         }
+
+//         return source_->get(index.get_finite_part());
+//     }
+
+// private:
+//     const Sequence<T>* source_;
+//     std::size_t index_ = 0;
+// };
+
