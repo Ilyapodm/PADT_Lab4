a209d03c1cc38694d47682b1752cdb76cbd71355
Ilya Podmarkov <shmell.letitt12@gmail.com>
2026-06-01 15:44:23 +0300

    feat: add set node

---
+++ src/set_node.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "node.hpp"
+#include <stdexcept>
+
+template <typename T>
+class SetNode : public Node<T> {
+public:
+    SetNode(const Node<T>& source, const T& value, const Ordinal& index) :
+        source_(nullptr),
+        value_(value),
+        index_(index)
+    {
+        if (index >= source.length())
+            throw std::invalid_argument("SetNode index out of range");
+        
+        source_ = source.clone();
+    }
+
+    SetNode(const SetNode& other) : SetNode(*other.source_, other.value_, other.index_) {}
+
+    SetNode& operator=(const SetNode& other) {
+        if (this == &other)
+            return *this;
+
+        Node<T>* new_source = other.source_->clone();
+        delete source_;
+
+        source_ = new_source;
+        value_ = other.value_;
+        index_ = other.index_;
+
+        return *this;
+    }
+
+    ~SetNode() override {
+        delete source_;
+    }
+
+    const Ordinal& length() const override {
+        return source_->length();
+    }
+
+    SetNode<T>* clone() const override {
+        return new SetNode<T>(*this);
+    }
+
+protected:
+    const T& value_at_impl(const Ordinal& index) override {
+        if (index == index_) 
+            return value_;
+
+        return source_->value_at(index);
+    }
+
+private:
+    Node<T>* source_;
+    T value_;
+    Ordinal index_;
+};
+
